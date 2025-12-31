a87c2bfadee1495e1957f0e8dae3937f926e67b0
prideicker <prideicker@gmail.com>
2025-12-31 08:33:30 +0800

    chore: apply .gitignore and remove ignored files from index

--- rabbish/count_script_references.ps1
+++
@@ -1,8 +0,0 @@
-$scripts = Get-ChildItem -Path 'D:\YouTube\FluentYTDL\scripts' -File | Select-Object -ExpandProperty Name
-foreach ($s in $scripts) {
-    $pattern = [regex]::Escape($s)
-    $matches = Select-String -Path 'D:\YouTube\FluentYTDL\**\*' -Pattern $pattern -SimpleMatch -ErrorAction SilentlyContinue
-    $external = $matches | Where-Object { $_.Path -notlike "*\scripts\$s" }
-    $count = ($external | Measure-Object).Count
-    Write-Host "$s -> external references: $count"
-}
